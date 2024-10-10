cc10c9b37277d39fd40f1ba1df78eee9b91233e8
NicholasIGuess <114117177+NicholasIGuess@users.noreply.github.com>
2024-10-10 13:43:26 -0400

    clean up

--- script.js
+++ script.js
@@ -47,55 +47,9 @@ function checkRent() {
 }
 
 
-function getRentDueTest(){
+function getRentDue(){
     var vendorName = document.getElementById("1952425390").value;
     vendorName = encodeURIComponent(vendorName);
     var url = "https://docs.google.com/spreadsheets/d/1z82wLqfuOacuHxwXeRoMhLzRnrUlEgKM0wgvSEZLgdw/gviz/tq?tqx=out:html&tq=select A, B, C where A=%22"+vendorName+"%22";
     document.getElementById("rentFrame").src = url;
-}
-
-
-
-function getRentDue(vendor){
-    let SHEET_ID = "1z82wLqfuOacuHxwXeRoMhLzRnrUlEgKM0wgvSEZLgdw";
-
-    // Creating a XHR object
-    let xhr = new XMLHttpRequest();
-    let myurl = "https://sheets.googleapis.com/v4/spreadsheets/"+SHEET_ID+"/values/Rent?valueRenderOption=FORMATTED_VALUE&key=AIzaSyA6gLJfCoxH3OxtpVzw7lWBqNkiEx6noGA"
-
-    // open a connection
-    xhr.open("GET", myurl, false);
-    //xhr.setRequestHeader('Access-Control-Allow-Credentials', 'true');
-
-    // Set the request header i.e. which type of content you are sending
-    xhr.setRequestHeader("Content-Type", "application/json");
-    var rentdue = "0"
-
-    // Create a state change callback
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-
-            // Print received data from server
-            // alert(xhr.responseText); // this.responseText;
-            const jsonString = xhr.responseText;
-            const json = JSON.parse(jsonString);
-            // alert(json.values.length);
-            for (var i=0; i < json.values.length; i++) {
-                row = json.values[i];
-                // alert(row[0]);
-                if(vendor == row[0]){
-                    // alert("match: "+row[1]+" ");
-                    if (row[1]) {
-                        rentdue = row[1];
-                    }
-                }
-             }
-                     
-        }
-    };
-
-
-    // Sending data with the request
-    xhr.send();
-    return rentdue;
 }
